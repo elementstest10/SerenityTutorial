0071deaa09a5ccd3b8a16f5def16d9a9ef33979f
trocky321 <tejasvi.qa@gmail.com>
2017-07-30 22:26:57 -0400

    Changing the port

--- StudentApp/src/test/java/com/studentapp/testbase/TestBase.java
+++ StudentApp/src/test/java/com/studentapp/testbase/TestBase.java
@@ -8,6 +8,6 @@ public class TestBase {
 
 	@BeforeClass
 	public static void init(){
-		RestAssured.baseURI = "http://localhost:8080/student";
+		RestAssured.baseURI = "http://localhost:8085/student";
 	}
 }
